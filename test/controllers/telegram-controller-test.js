458667d5882552fbd8f308b6553185e67b2b4d84
Fahrradflucht <mail@mathiswiehl.de>
2016-06-11 14:15:07 +0200

    Add pending test for /subscribe route

--- test/controllers/telegram-controller-test.js
+++ test/controllers/telegram-controller-test.js
@@ -31,12 +31,42 @@ const stubs = {
           "length": 6
         }]
       }
+    },
+    subscribe: {
+      "update_id": 807061712,
+      "message": {
+        "message_id": 3,
+        "from": {
+          "id": 97633461,
+          "first_name": "Mathis",
+          "username": "Mathis_Fahrradflucht"
+        },
+        "chat": {
+          "id": 97633461,
+          "first_name": "Mathis",
+          "username": "Mathis_Fahrradflucht",
+          "type": "private"
+        },
+        "date": 1465591070,
+        "text": "/subscribe @homer",
+        "entities": [{
+            "type": "bot_command",
+            "offset": 0,
+            "length": 10
+          },
+          {
+            "type": "mention",
+            "offset": 11,
+            "length": 6
+          }
+        ]
+      }
     }
   }
 }
 
-describe('Telegram controller', () => {
-  describe('general', () => {
+describe("Telegram controller", () => {
+  describe("general", () => {
     it('returns 200 ok if there is no message', () => {
       return request
         .post(webhook)
@@ -47,14 +77,14 @@ describe('Telegram controller', () => {
         })
     });
   });
-  describe('commands', () => {
-    describe('/start', () => {
+  describe("commands", () => {
+    describe("/start", () => {
       const payload = stubs.commands.start;
-      describe("if user doesn't exist", () => {
+      context("if user doesn't exist", () => {
         afterEach(() => {
           return knex('users').del();
         });
-        it('creates the user', () => {
+        it("creates the user", () => {
           return request
             .post(webhook)
             .send(payload)
@@ -65,7 +95,7 @@ describe('Telegram controller', () => {
                 }).should.finally.have.length(1);
             })
         });
-        it('responds with welcome message', () => {
+        it("responds with welcome message", () => {
           return request
             .post(webhook)
             .send(payload)
@@ -76,7 +106,7 @@ describe('Telegram controller', () => {
             })
         });
       });
-      describe("if user already exists", () => {
+      context("if user already exists", () => {
         before(() => {
           const { message } = payload;
           return knex('users').insert([{
@@ -107,6 +137,27 @@ describe('Telegram controller', () => {
             })
         });
       })
+    });
+    describe("/subscribe", () => {
+      const payload = stubs.commands.subscribe;
+      context("@user argument is present", () => {
+        context("user is not registered", () => {
+          it("responds with error message")
+        });
+        context("user is registered", () => {
+          context("user isn't already subscribed", () => {
+            it("adds user to subscribers");
+            it("notifies user that she/he got added to subscribers");
+            it("responds with success message");
+          });
+          context("user is already subscribed", () => {
+            it("responds with error message");
+          });
+        })
+      });
+      context("@user argument is missing", () => {
+        it('handles that case');
+      });
     });
   });
 });
